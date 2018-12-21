e1e2082c4353087e62f614ceec37a767928d0e9f
thereal-BLKMGK <44712995+thereal-BLKMGK@users.noreply.github.com>
2018-12-20 23:54:27 -0500

    added code to crop
    uncommented code to detect frames for now. Added code to detect for cropping however the value returned needs to be quoted I believe.

--- script.sh
+++ script.sh
@@ -9,14 +9,14 @@
 
 # parse mediainfo output for frame count, select number field, and 
 # strip whitespace
-#frameCount=`mediainfo --fullscan $1 | \
-#  grep -m 1 'Frame count' | \
-#  cut -d ':' -f 2 | \
-#  sed "s/ //"`
+frameCount=`mediainfo --fullscan $1 | \
+  grep -m 1 'Frame count' | \
+  cut -d ':' -f 2 | \
+  sed "s/ //"`
 
-#new framecount code
+#new framecount code - commented out for now
 
-frameCount=`mediainfo --fullscan --Output=JSON incred2.mkv | jq '.media.track[0].FrameCount''
+#frameCount=`mediainfo --fullscan --Output=JSON incred2.mkv | jq '.media.track[0].FrameCount''
 
 echo $framecount
 
@@ -32,8 +32,12 @@ startframe=0
 endframe=$jobsize
 counter=0
 
+#crop detection code cribbed elsewhere
+cropdetect="$(ffmpeg -ss 13 -i $1 -t 1 -vf "cropdetect=24:16:0" -preset ultrafast -f null - 2>&1 | awk '/crop/ { print $NF }' | tail -1)"
+
+
 while [ $counter -lt $jobnum ]; do
-  echo "ffmpeg -hide_banner -i "$1" -strict -1 -f yuv4mpegpipe - | x265 - --no-open-gop --chunk-start " $startframe" --colorprim bt709 --transfer bt709 --colormatrix bt709 --crf=20 --fps 24000/1001 --min-keyint 24 --keyint 240 --chunk-end " $endframe " --sar 1:1 --preset slow --ctu 16 --y4m --pools "+" -o chunky"$counter".265"
+  echo "ffmpeg -hide_banner -i "$1" -filter:v $cropdetect -strict -1 -f yuv4mpegpipe - | x265 - --no-open-gop --chunk-start " $startframe" --colorprim bt709 --transfer bt709 --colormatrix bt709 --crf=20 --fps 24000/1001 --min-keyint 24 --keyint 240 --chunk-end " $endframe " --sar 1:1 --preset slow --ctu 16 --y4m --pools "+" -o chunky"$counter".265"
   
   # calc next frame batch; truncate batch size if it exceeds
   # total frame count on last batch
