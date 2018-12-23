a671a11445adfa710e7199934b28dfaf10f78169
thereal-BLKMGK <44712995+thereal-BLKMGK@users.noreply.github.com>
2018-12-23 02:43:44 -0500

    Chunking doesn't work like I thought it would!
    Chunking reads the entire file it's given, it won't move to as keyframe. This update uses seek and frames to move through the file and then chunk with a 100 frame buffer on either side to grease the encoder. I believe it could be coded better for sure. 
    
    What we want is a see/frames window that moves through the video with chunk processing within it. I start at -100 so chunk starts at frame 0. It's completely possible 100 frames may not be a good number plus or minus. As you get deeper into the video seek takes longer at job start but it's normal.

--- script.sh
+++ script.sh
@@ -18,37 +18,44 @@ frameCount=`mediainfo --fullscan $1 | \
 
 #frameCount=`mediainfo --fullscan --Output=JSON incred2.mkv | jq '.media.track[0].FrameCount''
 
-echo $framecount
 
 # jobnum controls how many jobs we're making
-jobnum=60
+jobnum=6
 
 ## USER CONFIG VARS ##
 
 
 # jobsize is number of frames per job
-jobsize=$(( frameCount / jobnum ))
-startframe=0
-endframe=$jobsize
+jobsize=$(( frameCount / jobnum )) #Do we need to round this up?
+
 counter=0
+seek=(-100)
+frames=$(( jobsize + 100 )) #prime frames first stop
+
 
 #crop detection code cribbed elsewhere
 cropdetect="$(ffmpeg -ss 13 -i $1 -t 1 -vf "cropdetect=24:16:0" -preset ultrafast -f null - 2>&1 | awk '/crop/ { print $NF }' | tail -1)"
 
 
 while [ $counter -lt $jobnum ]; do
-  echo "ffmpeg -hide_banner -i "$1" -filter:v $cropdetect -strict -1 -f yuv4mpegpipe - | x265 - --no-open-gop --chunk-start " $startframe" --colorprim bt709 --transfer bt709 --colormatrix bt709 --crf=20 --fps 24000/1001 --min-keyint 24 --keyint 240 --chunk-end " $endframe " --sar 1:1 --preset slow --ctu 16 --y4m --pools "+" -o chunky"$counter".265"
+
+  echo "ffmpeg -hide_banner -i "$1" -filter:v "$cropdetect" -strict -1 -f yuv4mpegpipe - | x265 - --no-open-gop --seek $seek --frames $frames --chunk-start 100 --chunk-end $jobsize --colorprim bt709 --transfer bt709 --colormatrix bt709 --crf=20 --fps 24000/1001 --min-keyint 24 --keyint 240 --sar 1:1 --preset slow --ctu 16 --y4m --pools "+" -o chunky"$counter".265"
   
   # calc next frame batch; truncate batch size if it exceeds
   # total frame count on last batch
-  startframe=$(( startframe + jobsize ))
-  endframe=$(( endframe + jobsize ))
-  if [ $endframe -ge $frameCount ]; then
+  # seek should be 100 frames less than the last ending frame
+  seek=$(( (($seek - 100 )) + $frames ))
+  #seek=$(( $jobsize + 200 ))
+ endframe=$(( endframe + jobsize ))
+  
+  # Logic ERROR here - we are missing final frames in the last job. Too tired to fix.
+ if [ $endframe -ge $frameCount ]; then
     endframe=$frameCount
   fi
 
   ((counter++))
 
-  echo $startframe
-  echo $endframe
+# echo startframe $startframe
+ #echo endframe $endframe
 done
+
