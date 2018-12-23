8af8c252c0486ab0ef707451fcaf758ce3bc6ddb
thereal-BLKMGK <44712995+thereal-BLKMGK@users.noreply.github.com>
2018-12-23 17:17:29 -0500

    Still screwed
    First job needs to be unique. It needs to start at frame zero and go 100 frames past job size. All subsequent jobs need to seek from the previous stop -100 and then go +100 over the jobsize. Chunk start is 0 to start and then 100 afterwards. We should end up with a sliding window through the video with chunk encoding within a buffer that's 100 frames on either side. 
    
    ffmpeg -i INPUTFILE.MKV -f yuv4mpegpipe - | x265 --y4m - --crf 20 --no-open-gop --seek 0 --frames 1100 --chunk-start 0 --chunk-end 1000 chunk0.hevc
    
    ffmpeg -i INPUTFILE.MKV -f yuv4mpegpipe - | x265 --y4m - --crf 20 --no-open-gop --seek 900 --frames 1200 --chunk-start 100 --chunk-end 1100 chunk1.hevc
    
    ffmpeg -i INPUTFILE.MKV -f yuv4mpegpipe - | x265 --y4m - --crf 20 --no-open-gop --seek 1900 --frames 1200 --chunk-start 100 --chunk-end 1100 chunk2.hevc
    
    ffmpeg -i INPUTFILE.MKV -f yuv4mpegpipe - | x265 --y4m - --crf 20 --no-open-gop --seek 1900 --frames 1200 --chunk-start 101 --chunk-end 1100 chunk2.hevc

--- script.sh
+++ script.sh
@@ -14,48 +14,55 @@ frameCount=`mediainfo --fullscan $1 | \
   cut -d ':' -f 2 | \
   sed "s/ //"`
 
+  
 #new framecount code - commented out for now
 
 #frameCount=`mediainfo --fullscan --Output=JSON incred2.mkv | jq '.media.track[0].FrameCount''
 
+#echo framecount "$framecount"
 
 # jobnum controls how many jobs we're making
 jobnum=6
 
 ## USER CONFIG VARS ##
 
-
 # jobsize is number of frames per job
-jobsize=$(( frameCount / jobnum )) #Do we need to round this up?
+#jobsize=$(((( frameCount / jobnum )) + 1 )) #adding frames to make sure we run off the end - brute force kludge! If too many jobs are built we may have an issue, I'd prefer a sane rounding
 
+jobsize=1000 #changed for testing
+#echo jobsize $jobsize 
 counter=0
-seek=(-100)
-frames=$(( jobsize + 100 )) #prime frames first stop
+seek=0
+chunkstart=0
+frames=$(( jobsize + 100 )) #FIRST job gets an added 100 frames, after this we want 200
 
 
 #crop detection code cribbed elsewhere
 cropdetect="$(ffmpeg -ss 13 -i $1 -t 1 -vf "cropdetect=24:16:0" -preset ultrafast -f null - 2>&1 | awk '/crop/ { print $NF }' | tail -1)"
 
-
 while [ $counter -lt $jobnum ]; do
 
-  echo "ffmpeg -hide_banner -i "$1" -filter:v "$cropdetect" -strict -1 -f yuv4mpegpipe - | x265 - --no-open-gop --seek $seek --frames $frames --chunk-start 100 --chunk-end $jobsize --colorprim bt709 --transfer bt709 --colormatrix bt709 --crf=20 --fps 24000/1001 --min-keyint 24 --keyint 240 --sar 1:1 --preset slow --ctu 16 --y4m --pools "+" -o chunky"$counter".265"
+# chunkstart=$(( $seek + 100 ))
+# chunkend=$(( $chunkstart + $jobsize ))
+# echo
+# echo seek $seek then stream $frames frames
+# echo chunk starts $chunkstart and goes $jobsize forward to end $chunkend 
+
+	echo "ffmpeg -hide_banner -i "$1" -filter:v "\""$cropdetect"\"" -strict -1 -f yuv4mpegpipe - | x265 - --no-open-gop --seek $seek --frames $frames --chunk-start $chunkstart --chunk-end $jobsize --colorprim bt709 --transfer bt709 --colormatrix bt709 --crf=20 --fps 24000/1001 --min-keyint 24 --keyint 240 --sar 1:1 --preset slow --ctu 16 --y4m --pools "+" -o chunky"$counter".265"
+	
+ 
+  # seek should be 100 frames less than the last ending streamed frame
+  seek=$(((( $seek + $frames )) -100 )) 
+  chunkstart=100
+  frames=$(( jobsize + 100 )) #frames, total frames needs to be 200 over jobsize to allow a 100 frame buffer on either end of chunk except FIRST job. Seek gave us the first 100
   
-  # calc next frame batch; truncate batch size if it exceeds
-  # total frame count on last batch
-  # seek should be 100 frames less than the last ending frame
-  seek=$(( (($seek - 100 )) + $frames ))
-  #seek=$(( $jobsize + 200 ))
+  
  endframe=$(( endframe + jobsize ))
-  
-  # Logic ERROR here - we are missing final frames in the last job. Too tired to fix.
- if [ $endframe -ge $frameCount ]; then
-    endframe=$frameCount
-  fi
-
-  ((counter++))
-
-# echo startframe $startframe
+ 
  #echo endframe $endframe
+  
+   ((counter++))
+
+
 done
 
